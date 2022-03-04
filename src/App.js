0f97dbdb63a7c9b45176586399ae2b373e5eb54a
Jordi Galobart Salvat <jgalobart@gmail.com>
2022-03-04 21:33:26 +0100

    add environment variables

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
 import Contact from './components/contact';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-            <Contact></Contact>
-        </ul>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        fetch(process.env.REACT_APP_BACKEND_URL+'contacts')
+            .then((response) => response.json())
+            .then((json)=>console.log(json))
+    }, []);
+    
+
+    return (
+        <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+            Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            Learn React
+            </a>
+            <ul>
+                <Contact></Contact>
+            </ul>
+        </header>
+        </div>
+    );
 }
 
 export default App;
